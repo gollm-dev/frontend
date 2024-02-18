bd75ad7ea4d72c5245909e84d5bdb579cb46b102
Dru1DD <olexandrandrichuk@gmail.com>
2024-02-18 13:32:19 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+"use client";
+import {useEffect, useState} from "react";
+import { PT_Sans, Montserrat } from "next/font/google";
+import axios from "axios";
+
+const ptSans = PT_Sans({ subsets: ["latin"], weight: "400" });
+const monteserrat = Montserrat({subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    const [ modelName, setModelName ] = useState<string>("");
+
+    const [isHideInput, setIsHideInput] = useState<boolean>(false);
+
+    const handleClick = async() => {
+        setIsHideInput(true)
+        // try {
+        //     const response= await axios.post("https://api.gollm.xyz/downloadModel", {
+        //         model_name: modelName.replace(/\//g, '-_-')
+        //     });
+        //     if(response.status === 200) {
+        //         setIsHideInput(true)
+        //     }
+        // } catch(e) {
+        //     console.log(e);
+        // }
+    }
+
+    return (
+      <div
+          className={"relative max-w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-200 bg-blur to-[#fffccc] overflow-hidden"}>
+          <svg width="1000" height="1000" className={"transform -rotate-90 absolute top-[-450px] left-[-130px] "}>
+              <path d="M 250 100 C 200 100 100 250 150 350 Q 250 550 800 600 " fill="none" stroke="#fffccc"
+                    strokeWidth="50"/>
+          </svg>
+          <h1 className={`${monteserrat.className} mb-1 font-mono font-bold text-[10rem] text-black `}>
+              GOLLM!
+          </h1>
+          <h1 className={"mb-1 font-mono text-xl text-black"}>
+              Paste AI <br className="block md:hidden"/>
+              <span
+                  className="inline-flex h-[60px] pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform"
+              >
+      model name from HuggingFace
+    </span>
+              <span
+                  className="box-border inline-block w-1 h-4 ml-2 -mb-2 bg-white md:-mb-4 md:h-10 animate-cursor will-change-transform"
+              ></span>
+          </h1>
+          <div className={"absolute top-20 right-[300px] bg-[#f0c4ff] border border-black w-40 h-10 rounded-3xl p-6"}>
+
+          </div>
+          {
+              isHideInput ? <TelegramLinkComponent
+                  modelName={modelName}
+              /> : (  <div className="group flex flex-row w-[600px] mt-5">
+                  <div className="flex items-center justify-center w-full">
+                      <input
+                          id="10"
+                          type="text"
+                          placeholder={"deepset/roberta-base-squad2"}
+                          className="relative h-[50px] w-full rounded-3xl bg-green-200 text-black border border-black pl-4 pr-20 outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:drop-shadow-lg"
+                          value={modelName}
+                          onChange={(e) => setModelName(e.target.value)}
+                      />
+                      <button
+                          className="ml-2 h-[50px] w-20 rounded-3xl p-5 flex items-center justify-center bg-green-200 text-xs font-semibold text-black border border-black transition-all duration-200 ease-in-out ease-out"
+                          onClick={handleClick}
+                      >GO!
+                      </button>
+                  </div>
+              </div>)
+          }
+
+
+          <div
+              className={"w-[400px] h-[400px] bg-transparent border-[70px] border-pink-300  rounded-full absolute bottom-[-150px] right-[-100px]"}>
+
+          </div>
       </div>
+  )
+      ;
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const TelegramLinkComponent = ({ modelName } : {modelName: string} ) => {
+    const [timeLeft, setTimeLeft] = useState(5);
+    const [isDownloadedStatus, setIsDownloadedStatus] = useState<boolean>(false)
+    const totalSeconds = 5;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    // useEffect(() => {
+    //     const fetchInfo = async () => {
+    //         try {
+    //             const result = await axios.post("https://api.gollm.xyz/downloadStatus", {
+    //                 model_name: modelName.replace(/\//g, '-_-')
+    //             });
+    //
+    //             if(result.status === 200 && result.data.status === "Downloaded") {
+    //                 setIsDownloadedStatus(true);
+    //             }
+    //         } catch(e) {
+    //             console.log(e);
+    //         }
+    //     }
+    //
+    //     void fetchInfo();
+    // }, []);
+    useEffect(() => {
+        let intervalId: any;
+
+        if ( timeLeft > 0) {
+            intervalId = setInterval(() => {
+                setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [timeLeft]);
+
+    const percentage = ((totalSeconds - timeLeft) / totalSeconds) * 100;
+
+    if(timeLeft !== 0 && !isDownloadedStatus) {
+        return (
+            <div className="h-2 w-[800px] bg-neutral-300 rounded-3xl">
+                <div className="h-2 bg-purple-900 border border-black ease-linear rounded-3xl" style={{ width: `${percentage}%`, transition: 'width 1s ease-in-out'  }}></div>
+            </div>
+        )
+    }
+
+    return (
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            href={`https://t.me/gollm_bot?start=${modelName.replace(/\//g, '-_-')}`}
+           className={" bg-[#f0c4ff] border border-black w-40 h-10 rounded-3xl p-6 text-white flex justify-center items-center"}>
+            <>Start a Bot</>
+        </a >
+    )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,42 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        "focus-4px-primary-100":
+            "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #e8ebf8",
+        "focus-4px-gray-100":
+            "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #EEF2F6",
+        "focus-4px-error-100":
+            "0px 1px 2px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #FEE4E2",
+      },
+      keyframes: {
+        loadingFade: {
+          "0%": { opacity: "0", transform: "scale(1)" },
+          "50%": { opacity: "0.8", transform: "scale(1.3)" },
+          "100%": { opacity: "0", transform: "scale(1)" },
+        },
+          type: {
+              '0%': { width: '0ch' },
+              '5%, 10%': { width: '1ch' },
+              '15%, 20%': { width: '2ch' },
+              '25%, 30%': { width: '3ch' },
+              '35%, 40%': { width: '4ch' },
+              '45%, 50%': { width: '5ch' },
+              '55%, 60%': { width: '6ch' },
+              '65%, 70%': { width: '7ch' },
+              '75%, 80%': { width: '8ch' },
+              '85%, 90%': { width: '9ch' },
+              '95%': { width: '10ch' },
+          },
+      },
+       animation: {
+        "typing-1": "loadingFade 1s infinite 300ms",
+        "typing-2": "loadingFade 1s infinite 400ms",
+        "typing-3": "loadingFade 1s infinite 500ms",
+            cursor: 'cursor .6s linear infinite alternate',
+            type: 'type 1.8s ease-out .8s 1 normal both',
+            'type-reverse': 'type 1.8s ease-out 0s infinite alternate-reverse both',
+      },
     },
   },
   plugins: [],
